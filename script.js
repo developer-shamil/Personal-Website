e1038439651e84792e1daafa3e8f7873ea1a70c6
Shamil KP <shamilkp7895@gmail.com>
2025-03-03 14:45:20 +0530

    js file

---
+++ script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    $('#form').validate({
+        errorClass: 'error',
+        validClass: 'valid',
+        rules: {
+            fname: {
+                required: true,
+                minlength: 4,
+                maxlength: 10
+            },
+            lastname: {
+                required: true,
+                minlength: 2,
+                maxlength: 10
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            message: {
+                required: true,
+            }
+        }
+    })
+
+    $("#form").submit((e)=>{
+        e.preventDefault()
+        $.ajax({
+            url:"https://script.google.com/macros/s/AKfycbx42jSzCz-nQjhLF9_3SHbssS9txcFnXGdu07w4P1sc25BM3zKz3HvLzW5KsjXU0SZm/exec",
+            data:$("#form").serialize(),
+            method:"post",
+            success:function (response){
+                window.location.reload()
+                //window.location.href="https://google.com"
+            },
+            error:function (err){
+                alert("Something Error")
+
+            }
+        })
+    })
+
+    $("#SubmitButton").click(function() {
+        var fname = $("#fname").val()
+        var lname = $("#lastname").val()
+        var email = $("#email").val()
+        var message = $("#message").val()
+
+        if(fname == '' || lname == '' || email == '' || message == '') {
+            swal({
+                title: "Fields Empty!",
+                text: "Please Check The Missing Fields!",
+                icon: "warning",
+                button: "Ok",
+            })
+        }
+        else {
+            swal({
+                title: "Congrarulations Your Contact Form is Submited",
+                icon: "success",
+                button: "Ok",
+            })
+        }
+    })
+})
